726e010efc089a1dadcab1c8e7ab1c9d511fa0e6
luispantoja1102 <l20211821@tectijuana.edu.mx>
2024-05-14 11:50:24 -0700

    Form

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
-
+ import { ref } from 'vue'
+ import Header from './components/Header.vue';
+ import Formulario from './components/Formulario.vue';
 </script>
 
 <template>
-  <h1 class="text-6xl">Hola mundo</h1>
+  <div class="container mx-auto mt-20">
+    <Header />
+    <div class="mt-12 md:flex">
+      <Formulario />
+      
+    </div>
+  </div>
 </template>
 
---
+++ src/components/Alerta.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        Es una Alerta
+    </div>
+</template>
